685b768d485bc8e9d50bc7ae429ba0a9750afd67
Benjamin Toll <benjam72@yahoo.com>
2020-02-28 20:13:51 -0500

    Shoehorn `test_package.sh` into the test phase

--- scripts/release/test/stage/test/task.sh
+++ scripts/release/test/stage/test/task.sh
@@ -8,17 +8,25 @@ echo
 date "+build_release begin TEST stage %Y%m%d_%H%M%S"
 echo
 
-echo Setup GPG
-#"${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/gpg-fake.sh
-"${HOME}"/ben-branch/scripts/release/test/gpg-fake.sh
+echo Setup GPG.
+#"${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/util/gpg-fake.sh
+"${HOME}"/ben-branch/scripts/release/test/util/gpg-fake.sh
 
 #"${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/deb/run_ubuntu.sh
 "${HOME}"/ben-branch/scripts/release/test/deb/run_ubuntu.sh
 date "+build_release done testing ubuntu %Y%m%d_%H%M%S"
 
 #"${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/rpm/run_centos.sh
-"${HOME}"/ben-branch//scripts/release/test/rpm/run_centos.sh
+"${HOME}"/ben-branch/scripts/release/test/rpm/run_centos.sh
 date "+build_release done testing centos %Y%m%d_%H%M%S"
+
+echo Use Docker to perform a smoke test.
+#pushd "${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/util
+pushd "${HOME}"/ben-branch/scripts/release/test/util
+# Copy all packages to the same directory where the Dockerfile will reside.
+cp "${HOME}"/node_pkg/* .
+./test_package.sh
+popd
 
 echo
 date "+build_release end TEST stage %Y%m%d_%H%M%S"
