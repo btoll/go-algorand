366adfb75c72115f2fba9fabdb7aea8cd9478f61
Benjamin Toll <benjam72@yahoo.com>
2020-04-02 16:58:21 -0400

    Remove call to build_deb.sh from build_packages.sh

--- scripts/release/build/deb/package.sh
+++ scripts/release/build/deb/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=1090
+# shellcheck disable=1090,2064
 
 echo
 date "+build_release begin PACKAGE DEB stage %Y%m%d_%H%M%S"
@@ -9,12 +9,38 @@ echo
 
 set -ex
 
-export BUILD_DEB=1
 export NO_BUILD=1
 export GOPATH="${HOME}"/go
 export PATH="${GOPATH}":/usr/local/go/bin:"${PATH}"
+
 pushd "${REPO_ROOT}"
+if [ "${CHANNEL}" = "" ]
+then
+    if [ "${BRANCH}" = "" ]
+    then
+        BRANCH=$(./scripts/compute_branch.sh)
+        export BRANCH
+    fi
+    CHANNEL=$(./scripts/compute_branch_channel.sh "${BRANCH}")
+    export CHANNEL
+fi
+
 ./scripts/build_packages.sh "${PLATFORM}"
+
+DEBTMP=$(mktemp -d 2>/dev/null || mktemp -d -t "debtmp")
+trap "rm -rf ${DEBTMP}" 0
+
+PLATFORM_SPLIT=(${PLATFORM//\// })
+OS=${PLATFORM_SPLIT[0]}
+ARCH=${PLATFORM_SPLIT[1]}
+PKG_NAME=${OS}-${ARCH}
+if ! ./scripts/build_deb.sh "${ARCH}" "${DEBTMP}" "${CHANNEL}"
+then
+    echo "Error building debian package for ${PLATFORM}.  Aborting..."
+    exit 1
+fi
+
+cp -p "${DEBTMP}/*.deb" "${PKG_ROOT}/algorand_${CHANNEL}_${PKG_NAME}_${FULLVERSION}.deb"
 popd
 
 # build docker release package
